a665e144e658271fa004fa8cbda8cecab6f491d1
hybridauth <hybridauth@gmail.com>
2011-12-04 15:33:47 +0100

    improve install

--- hybridauth/install.php
+++ hybridauth/install.php
@@ -112,38 +112,47 @@ ul li label {
 	$PROVIDERS_CONFIG      = ARRAY(
 								ARRAY( 
 									"label"             => "Facebook",
-									"new_app_link"      => "https://www.facebook.com/developers/apps.php",
+									"provider_name"     => "Facebook", 
+									"require_client_id" => TRUE, 
+									"new_app_link"      => "https://www.facebook.com/developers/",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_Facebook.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "Google",
+									"provider_name"     => "Google", 
 									"callback"          => TRUE,
+									"require_client_id" => TRUE, 
 									"new_app_link"      => "https://code.google.com/apis/console/",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_Google.html",
 								) 
 								,
 								ARRAY( 
 									"label"             => "Twitter",
-									"callback"          => TRUE,
+									"provider_name"     => "Twitter",  
 									"new_app_link"      => "https://dev.twitter.com/apps",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_Twitter.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "Live",
+									"provider_name"     => "Windows Live", 
+									"require_client_id" => TRUE, 
 									"new_app_link"      => "https://manage.dev.live.com/ApplicationOverview.aspx",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_Live.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "MySpace",
+									"provider_name"     => "MySpace", 
 									"new_app_link"      => "http://www.developer.myspace.com/",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_MySpace.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "Foursquare",
+									"provider_name"     => "Foursquare", 
+									"require_client_id" => TRUE, 
 									"callback"          => TRUE,
 									"new_app_link"      => "https://www.foursquare.com/oauth/",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_Foursquare.html",
@@ -151,25 +160,28 @@ ul li label {
 								,
 								ARRAY( 
 									"label"             => "LinkedIn",
-									"callback"          => TRUE,
-									"new_app_link"      => "https://www.linkedin.com/secure/developer?newapp=",
+									"provider_name"     => "LinkedIn",  
+									"new_app_link"      => "https://www.linkedin.com/secure/developer",
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_LinkedIn.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "OpenID",
+									"provider_name"     => "OpenID", 
 									"new_app_link"      => NULL,
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_OpenID.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "Yahoo",
+									"provider_name"     => "Yahoo!", 
 									"new_app_link"      => NULL,
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_Yahoo.html",
 								)
 								,
 								ARRAY( 
 									"label"             => "AOL",
+									"provider_name"     => "AOL", 
 									"new_app_link"      => NULL,
 									"userguide_section" => "http://hybridauth.sourceforge.net/userguide/IDProvider_info_AOL.html",
 								)
@@ -234,9 +246,9 @@ ul li label {
 <div id="content"> 
 	<?php
 		// check if php 5+. well donno the exact version to test, because it depend on which providers will be used..
-		if ( version_compare( PHP_VERSION, '5.0.0', '<' ) ):
+		if ( version_compare( PHP_VERSION, '5.2', '<=' ) ):
 	?>
-		<p style='background-color:#EE3322;color:#FFFFFF;margin:1em 0;padding:0.8em;border:1px #C52F24 solid;'><strong>Error: </strong> HybridAuth requires PHP 5 or higher</p>
+		<p style='background-color:#EE3322;color:#FFFFFF;margin:1em 0;padding:0.8em;border:1px #C52F24 solid;'><strong>Error: </strong> HybridAuth requires PHP 5.2 or higher</p>
 	<?php
 		endif;
 	?> 
@@ -276,8 +288,8 @@ ul li label {
 
 	<ul style="list-style:disc inside;">
 		<li style="color: #000000;font-size: 14px;">For security reason, please delete ("<b>install.php</b>") file as soon as you complete the installation process,</li>
-		<li style="color: #000000;font-size: 14px;">Using the HybridAuth installer will erase your the existen configuration file. If you already have an old installation of HybridAuth you might keep a copy of <b>config.php</b> file,</li>
-		<li style="color: #000000;font-size: 14px;">HybridAuth include by default 8 of the major social networks and identities providers. If you want more providers, please goto to HybridAuth web site and download the <a href="http://hybridauth.sourceforge.net/download.html">Additional Providers Package</a>.</li>
+		<li style="color: #000000;font-size: 14px;">Using the HybridAuth installer will erase your the existen configuration file. If you already have an old installation of HybridAuth you might want to keep a copy of <b>config.php</b> file,</li>
+		<li style="color: #000000;font-size: 14px;">HybridAuth include by default <?php echo count( $PROVIDERS_CONFIG ) + 1 ?> providers. If you want even more, please goto to HybridAuth web site and download the <a href="http://hybridauth.sourceforge.net/download.html">Additional Providers Package</a>.</li>
 		<li style="color: #000000;font-size: 14px;">Visit <a href="http://hybridauth.sourceforge.net/#installer">HybridAuth</a> Home page to make sure if there is any newer version.</li>
 	</ul> 
  
@@ -317,6 +329,8 @@ ul li label {
 	
 	foreach( $PROVIDERS_CONFIG AS $item ):
 		$provider                   = @ $item["label"];
+		$provider_name              = @ $item["provider_name"];
+		$require_client_id          = @ $item["require_client_id"];
 		$provider_new_app_link      = @ $item["new_app_link"];
 		$provider_userguide_section = @ $item["userguide_section"];
 		$provider_callback_url      = "" ;
@@ -325,21 +339,21 @@ ul li label {
 			$provider_callback_url  = '<span style="color:green">' . $GLOBAL_HYBRID_AUTH_URL_BASE . '?hauth.done=' . $provider . '</span>';
 		}
 
-		$nb_provider                ++;
+		$setupsteps = 0;
 ?>
-	<h4 style="margin-left:10px;"><?php echo $nb_provider ?> - <?php echo $provider ?></h4> 
+	<h3 style="margin-left:30px;"><?php echo $provider_name ?></h3> 
 	<div> 
 		<div class="cfg">
 		   <div class="cgfparams">
 			  <ul>
-				 <li><label><?php echo $provider ?> Adapter Satus</label>
+				 <li><label><?php echo $provider_name ?> Adapter Satus</label>
 					<select name="<?php echo strtoupper( $provider ) ?>_ADAPTER_STATUS">
 						<option selected="selected" value="true">Enabled</option>
 						<option value="false">Disabled</option>
 					</select>
 				</li>
 				<?php if ( $provider_new_app_link ) : ?>
-					<?php if ( in_array( $provider, array( "Facebook", "Google", "Live", "Foursquare" ) ) ) : ?>
+					<?php if ( $require_client_id ) : ?>
 						<li><label>Application ID</label><input type="text" class="inputgnrc" value="" name="<?php echo strtoupper( $provider ) ?>_APPLICATION_APP_ID"    ></li>
 					<?php else: ?>	
 						<li><label>Application Key</label><input type="text" class="inputgnrc" value="" name="<?php echo strtoupper( $provider ) ?>_APPLICATION_KEY"    ></li>
@@ -349,35 +363,50 @@ ul li label {
 			  </ul> 
 		   </div>
 		   <div class="cgftip">
-				<?php if ( $provider_new_app_link  ) : ?>
-					<p>In order to set up <?php echo $provider ?>, you must register your website with <?php echo $provider ?> at <a href="<?php echo $provider_new_app_link ?>" target ="_blanck"><?php echo $provider_new_app_link ?></a></p>
-	 
+				<?php if ( $provider_new_app_link  ) : ?> 
+					<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Go to <a href="<?php echo $provider_new_app_link ?>" target ="_blanck"><?php echo $provider_new_app_link ?></a> and <b>create a new application</b>.</p>
+
+					<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Fill out any required fields such as the application name and description.</p>
+
+					<?php if ( $provider == "google" ) : ?>
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> On the <b>"Create Client ID"</b> popup switch to advanced settings by clicking on <b>(more options)</b>.</p>
+					<?php endif; ?>	
+
 					<?php if ( $provider_callback_url ) : ?>
-						<p>Provide <?php echo $provider_callback_url ?> as the Callback URL for your application.</p>
+						<p>
+							<?php echo "<b>" . ++$setupsteps . "</b>." ?> Provide this URL as the Callback URL for your application:
+							<br />
+							<?php echo $provider_callback_url ?>
+						</p>
 					<?php endif; ?> 
 
-					<?php if ( $provider == "MySpace" ) : ?>
-						<p>Make sure to put your correct website adress in the "External Url" and "External Callback Validation" fields. This adresse must match with the current hostname "<em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>".</p>
+					<?php if ( $provider == "myspace" ) : ?>
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Put your website domain in the <b>External Url</b> and <b>External Callback Validation</b> fields. This should match with the current hostname <em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>.</p>
 					<?php endif; ?> 
 
-					<?php if ( $provider == "Live" ) : ?>
-						<p>Make sure to put your correct website adress in the "Redirect Domain" field. This adresse must match with the current hostname "<em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>".</p>
+					<?php if ( $provider == "live" ) : ?>
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Put your website domain in the <b>Redirect Domain</b> field. This should match with the current hostname <em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>.</p>
 					<?php endif; ?> 
 
-					<?php if ( $provider == "Facebook" ) : ?>
-						<p>Make sure to put your correct website adress in the "Site Url" field. This adresse must match with the current hostname "<em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>".</p>
-						<p>Once you have registered, copy the created application ID and Secret into this setup page.</p> 
-					<?php elseif ( $provider == "Google" ) : ?>
-						<p>On the <b>"Create Client ID"</b> popup switch to advanced settings by clicking on <b>(more options)</b>.</p>
-						<p>Once you have registered, copy the created application client ID and client secret into this setup page.</p> 
-					<?php else: ?>	
-						<p>Once you have registered, copy the created application consumer key and Secret into this setup page.</p> 
-					<?php endif; ?>
+					<?php if ( $provider == "facebook" ) : ?>
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Put your website domain in the <b>Site Url</b> field. This should match with the current hostname <em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>.</p> 
+					<?php endif; ?>	
+
+					<?php if ( $provider == "linkedin" ) : ?>
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Put your website domain in the <b>Integration URL</b> field. This should match with the current hostname <em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>.</p> 
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Set the <b>Application Type</b> to <em style="color:#CB4B16;">Web Application</em>.</p> 
+					<?php endif; ?>	
+
+					<?php if ( $provider == "twitter" ) : ?>
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Put your website domain in the <b>Application Website</b> and <b>Application Callback URL</b> fields. This should match with the current hostname <em style="color:#CB4B16;"><?php echo $_SERVER["SERVER_NAME"] ?></em>.</p> 
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Set the <b>Application Type</b> to <em style="color:#CB4B16;">Browser</em>.</p> 
+						<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Set the <b>Default Access Type</b> to <em style="color:#CB4B16;">Read, Write, & Direct Messages</em>.</p> 
+					<?php endif; ?>	
+					
+					<p><?php echo "<b>" . ++$setupsteps . "</b>." ?> Once you have registered, copy and past the created application credentials into this setup page.</p>  
 				<?php else: ?>	
 					<p>No registration required for OpenID based providers</p> 
-				<?php endif; ?>
-
-				For more informations check out <a href="<?php echo $provider_userguide_section ?>" target ="_blanck"><?php echo $provider ?> adapter specifications</a> section on the online userguide. 
+				<?php endif; ?> 
 		   </div>
 		</div>   
 	</div> 
