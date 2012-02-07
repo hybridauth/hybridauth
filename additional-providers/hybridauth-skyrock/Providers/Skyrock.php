daf550223453aa1853236e4fde1fea27faf9a377
niQo <niQo @ NOSPAM>
2012-02-08 00:18:12 +0100

    clean up extra spaces

--- additional-providers/hybridauth-skyrock/Providers/Skyrock.php
+++ additional-providers/hybridauth-skyrock/Providers/Skyrock.php
@@ -6,7 +6,7 @@
 * http://hybridauth.sourceforge.net | https://github.com/hybridauth/hybridauth
 *  (c) 2009-2012 HybridAuth authors | hybridauth.sourceforge.net/licenses.html
 *
-* 
+*
 */
 
 /**
@@ -14,8 +14,8 @@
 */
 class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 {
-   	/**
-	* IDp wrappers initializer 
+	/**
+	* IDp wrappers initializer
 	*/
 	function initialize()
 	{
@@ -30,7 +30,7 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 		$this->api->curl_auth_header  = false;
 	}
 
-   /**
+	/**
 	* load the user profile from the IDp api client
 	*/
 	function getUserProfile()
@@ -47,29 +47,29 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 			throw new Exception( "User profile request failed! {$this->providerId} api returned an invalid response.", 6 );
 		}
 
-		# store the user profile.  
-		$this->user->profile->identifier  = (property_exists($response,'id_user'))?$response->id_user:"";
-		$this->user->profile->displayName = (property_exists($response,'username'))?$response->username:"";
-		$this->user->profile->webSiteURL  = (property_exists($response,'blog_url'))?$response->blog_url:"";
-		$this->user->profile->profileURL  = (property_exists($response,'profile_url'))?$response->profile_url:"";
-		$this->user->profile->photoURL    = (property_exists($response,'avatar_url'))?$response->avatar_url:"";
-//unknown		$this->user->profile->description = (property_exists($response,'description'))?$response->description:"";
-		$this->user->profile->firstName   = (property_exists($response,'firstname'))?$response->firstname:"";
-		$this->user->profile->lastName    = (property_exists($response,'name'))?$response->name:"";
+		# store the user profile.
+		$this->user->profile->identifier    = (property_exists($response,'id_user'))?$response->id_user:"";
+		$this->user->profile->displayName   = (property_exists($response,'username'))?$response->username:"";
+		$this->user->profile->webSiteURL    = (property_exists($response,'blog_url'))?$response->blog_url:"";
+		$this->user->profile->profileURL    = (property_exists($response,'profile_url'))?$response->profile_url:"";
+		$this->user->profile->photoURL      = (property_exists($response,'avatar_url'))?$response->avatar_url:"";
+//unknown		$this->user->profile->description   = (property_exists($response,'description'))?$response->description:"";
+		$this->user->profile->firstName     = (property_exists($response,'firstname'))?$response->firstname:"";
+		$this->user->profile->lastName      = (property_exists($response,'name'))?$response->name:"";
 
-		if( property_exists($response,'gender') ) { 
+		if( property_exists($response,'gender') ) {
 			if( $response->gender == 1 ){
-		        	$this->user->profile->gender = "male";
+				$this->user->profile->gender = "male";
 			}
 			elseif( $response->gender == 2 ){
-			        $this->user->profile->gender = "female";
+				$this->user->profile->gender = "female";
 			}
 			else{
-			        $this->user->profile->gender = "";
+				$this->user->profile->gender = "";
 			}
 		}
 
-		$this->user->profile->language      = (property_exists($response,'lang'))?$response->lang:"";
+		$this->user->profile->language    = (property_exists($response,'lang'))?$response->lang:"";
 
 		if( property_exists( $response,'birth_date' ) && $response->birth_date ) {
                         $birthday = date_parse($response->birth_date);
@@ -78,27 +78,27 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 			$this->user->profile->birthYear  = $birthday["year"];
 		}
 
-		$this->user->profile->email      = (property_exists($response,'email'))?$response->email:"";
-		$this->user->profile->emailVerified      = (property_exists($response,'email'))?$response->email:"";
+		$this->user->profile->email         = (property_exists($response,'email'))?$response->email:"";
+		$this->user->profile->emailVerified = (property_exists($response,'email'))?$response->email:"";
 
 //unknown		$this->user->profile->phone      = (property_exists($response,'unknown'))?$response->unknown:"";
-		$this->user->profile->address      = (property_exists($response,'address1'))?$response->address1:"";
-		$this->user->profile->address     .= (property_exists($response,'address2'))?$response->address2:"";
-		$this->user->profile->country      = (property_exists($response,'country'))?$response->country:"";
+		$this->user->profile->address       = (property_exists($response,'address1'))?$response->address1:"";
+		$this->user->profile->address      .= (property_exists($response,'address2'))?$response->address2:"";
+		$this->user->profile->country       = (property_exists($response,'country'))?$response->country:"";
 //unknown		$this->user->profile->region      = (property_exists($response,'unknown'))?$response->unknown:"";
-		$this->user->profile->city      = (property_exists($response,'city'))?$response->city:"";
-		$this->user->profile->zip      = (property_exists($response,'postalcode'))?$response->postalcode:"";
+		$this->user->profile->city          = (property_exists($response,'city'))?$response->city:"";
+		$this->user->profile->zip           = (property_exists($response,'postalcode'))?$response->postalcode:"";
 
 		return $this->user->profile;
- 	}
+	}
 
-   	/**
+	/**
 	* load the current user contacts
 	*/
-	function getUserContacts() 
+	function getUserContacts()
 	{
 		$parameters = array( 'page' => 1 );
-		$response  = $this->api->get( 'user/list_friends.json', $parameters ); 
+		$response  = $this->api->get( 'user/list_friends.json', $parameters );
 
 		// check the last HTTP status code returned
 		if ( $this->api->http_code != 200 ){
@@ -111,14 +111,14 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 
 
 		if( $response && count( $response->friends ) ){
-			foreach( $response->friends as $item ){ 
+			foreach( $response->friends as $item ){
 				$uc = new Hybrid_User_Contact();
 
 				$uc->identifier   = (property_exists($item,'id_user'))?$item->id_user:"";
 				$uc->displayName  = (property_exists($item,'username'))?$item->username:"";
 				$uc->profileURL   = (property_exists($item,'profile_url'))?$item->profile_url:"";
 				$uc->photoURL     = (property_exists($item,'avatar_url'))?$item->avatar_url:"";
-				//$uc->description  = (property_exists($item,'description'))?$item->description:""; 
+				//$uc->description  = (property_exists($item,'description'))?$item->description:"";
 
 				$contacts[] = $uc;
 			}
@@ -129,14 +129,14 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 		for ($i = 1; $i<$max_page; $i++) {
 
 			$parameters = array( 'page' => $i );
-			$response  = $this->api->get( 'user/list_friends.json', $parameters ); 
+			$response  = $this->api->get( 'user/list_friends.json', $parameters );
 			// check the last HTTP status code returned
 			if ( $this->api->http_code != 200 ){
 				throw new Exception( "User contacts request failed! {$this->providerId} returned an error. " . $this->errorMessageByStatus( $this->api->http_code ) );
 			}
 
 			if( $response && count( $response->friends ) ){
-				foreach( $response->friends as $item ){ 
+				foreach( $response->friends as $item ){
 					$uc = new Hybrid_User_Contact();
 
 					$uc->identifier   = (property_exists($item,'id_user'))?$item->id_user:"";
@@ -150,7 +150,7 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 					}
 
 					$uc->photoURL     = (property_exists($item,'avatar_url'))?$item->avatar_url:"";
-					//$uc->description  = (property_exists($item,'description'))?$item->description:""; 
+					//$uc->description  = (property_exists($item,'description'))?$item->description:"";
 
 					$contacts[] = $uc;
 				}
@@ -158,18 +158,18 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 		}
 
 		return $contacts;
- 	}
+	}
 
-   	/**
-	* return the user activity stream  
+	/**
+	* return the user activity stream
 	*/
-	function getUserActivity( $stream ) 
+	function getUserActivity( $stream )
 	{
 		if( $stream == "me" ){
-			$response  = $this->api->get( 'newsfeed/list_events.json?events_category=own' ); 
-		}                                                          
+			$response  = $this->api->get( 'newsfeed/list_events.json?events_category=own' );
+		}
 		else{
-			$response  = $this->api->get( 'newsfeed/list_events.json?events_category=friends' ); 
+			$response  = $this->api->get( 'newsfeed/list_events.json?events_category=friends' );
 		}
 
 		// check the last HTTP status code returned
@@ -201,7 +201,7 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 			}
 
 			$ua->user->photoURL     = (property_exists($item->from,'avatar_url'))?$item->from->avatar_url:"";
-			
+
 			$activities[] = $ua;
 		}
 
@@ -211,7 +211,7 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 
 	/**
 	* update user status
-	*/ 
+	*/
 	function setUserStatus( $status )
 	{
 		$parameters = array( 'message' => $status );
@@ -221,6 +221,7 @@ class Hybrid_Providers_Skyrock extends Hybrid_Provider_Model_OAuth1
 		if ( $this->api->http_code != 200 ){
 			throw new Exception( "Update user status failed! {$this->providerId} returned an error. " . $this->errorMessageByStatus( $this->api->http_code ) );
 		}
- 	}
+	}
 
 }
+
