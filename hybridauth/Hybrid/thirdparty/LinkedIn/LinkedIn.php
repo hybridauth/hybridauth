c0e68d5791d787846de5592213fa22aa9790924b
Ben Squire <b.squire@gmail.com>
2013-04-11 15:46:01 +0100

    Fix so that LinkedIn CURL request honours proxy parameter in the config

--- hybridauth/Hybrid/thirdparty/LinkedIn/LinkedIn.php
+++ hybridauth/Hybrid/thirdparty/LinkedIn/LinkedIn.php
@@ -670,6 +670,10 @@ class LinkedIn {
       curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, FALSE);
       curl_setopt($handle, CURLOPT_URL, $url);
       curl_setopt($handle, CURLOPT_VERBOSE, FALSE);
+
+      if ( isset ( Hybrid_Auth::$config["proxy"] ) ) {
+      	curl_setopt($handle, CURLOPT_PROXY, Hybrid_Auth::$config["proxy"]);
+      }
       
       // configure the header we are sending to LinkedIn - http://developer.linkedin.com/docs/DOC-1203
       $header = array($oauth_req->to_header(self::_API_OAUTH_REALM));
