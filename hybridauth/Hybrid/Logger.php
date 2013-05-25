0d63d53299236349decfb2b2aa1259adfaebe2ea
César D. Rodas <crodas@php.net>
2013-05-25 01:38:12 -0400

    Update Logger.php
    Fixed little typo

--- hybridauth/Hybrid/Logger.php
+++ hybridauth/Hybrid/Logger.php
@@ -15,7 +15,7 @@ class Hybrid_Logger
 		// if debug mode is set to true, then check for the writable log file
 		if ( Hybrid_Auth::$config["debug_mode"] ){
 			if ( ! file_exists( Hybrid_Auth::$config["debug_file"] ) ){
-				throw new Exception( "'debug_mode' is set to 'true', but the file " . Hybrid_Auth::$config['debug_file'] . " in 'debug_file' does not exit.", 1 );
+				throw new Exception( "'debug_mode' is set to 'true', but the file " . Hybrid_Auth::$config['debug_file'] . " in 'debug_file' does not exist.", 1 );
 			}
 
 			if ( ! is_writable( Hybrid_Auth::$config["debug_file"] ) ){
