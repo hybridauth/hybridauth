2b84c2a7089ee18e9183c438e33a4825dd02ada3
Jarrett Hawrylak <jarrett.hawrylak@showclix.com>
2013-08-05 11:34:18 -0400

    woops, again

--- src/Hybridauth/Entity/Facebook/Profile.php
+++ src/Hybridauth/Entity/Facebook/Profile.php
@@ -1,8 +1,6 @@
 <?php
 namespace \Hybrid\Entity\Facebook;
 
-use \Hybrid\Entity\Profile;
-
 class Profile extends \Hybrid\Entity\Profile {
     function getPhotoUrl($width = 150, $height = 150) {
         return isset($this->photoURL) ?
