2ce01b862d3f560e0b6ca46c697a720a8c8dbc35
vovikturntwotech <volodymyr@turntwotech.com>
2014-03-24 15:42:23 -0400

    Update Endpoint.php
    See #132 "Serialization of 'Closure' is not allowed" on Google/Facebook cancel.

--- hybridauth/Hybrid/Endpoint.php
+++ hybridauth/Hybrid/Endpoint.php
@@ -176,7 +176,7 @@ class Hybrid_Endpoint {
 		}
 		catch( Exception $e ){
 			Hybrid_Logger::error( "Exception:" . $e->getMessage(), $e );
-			Hybrid_Error::setError( $e->getMessage(), $e->getCode(), $e->getTraceAsString(), $e );
+			Hybrid_Error::setError( $e->getMessage(), $e->getCode(), $e->getTraceAsString(), $e->getPrevious());
 
 			$hauth->adapter->setUserUnconnected(); 
 		}
