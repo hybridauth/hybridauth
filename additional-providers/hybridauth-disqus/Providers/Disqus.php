d1a5c4921e448cf05b6396f3d2b851f46a8122a0
Filippo Rossi <legend_k@live.it>
2014-06-16 21:26:14 +0200

    Updated Disqus provider (now works fine)

--- additional-providers/hybridauth-disqus/Providers/Disqus.php
+++ additional-providers/hybridauth-disqus/Providers/Disqus.php
@@ -2,17 +2,17 @@
 /*!
 * HybridAuth
 * http://hybridauth.sourceforge.net | https://github.com/hybridauth/hybridauth
-*  (c) 2009-2011 HybridAuth authors | hybridauth.sourceforge.net/licenses.html
+*  (c) 2009-2014 HybridAuth authors | hybridauth.sourceforge.net/licenses.html
 */
 
 /**
- * Hybrid_Providers_Disqus - DO NOT USE, does not work yet
- * says wrong API version, wrote a mail to Disqus support
+ * Hybrid_Providers_Disqus
  */
 class Hybrid_Providers_Disqus extends Hybrid_Provider_Model_OAuth2
 { 
-	// default permissions  
-	public $scope = "read,write";
+	// default permissions
+	// (read,email) => public info and email
+	public $scope = "read,email";
 
 	/**
 	* IDp wrappers initializer 
@@ -20,12 +20,12 @@ class Hybrid_Providers_Disqus extends Hybrid_Provider_Model_OAuth2
 	function initialize() 
 	{
 		parent::initialize();
-		// Provider api end-points
-		$this->api->api_base_url  = "https://disqus.com/api/";
-		$this->api->authorize_url = "https://disqus.com/api/oauth/2.0/authorize";	   
-		$this->api->token_url     = "https://disqus.com/api/oauth/2.0/access_token";
 		
-		$this->api->curl_header = array( 'client_id: ' . $this->config["keys"]["id"], 'Accept: application/json' );
+		// Provider api end-points
+		$this->api->api_base_url  = "https://disqus.com/api/3.0/";
+		$this->api->authorize_url = "https://disqus.com/api/oauth/2.0/authorize";	   
+		$this->api->token_url     = "https://disqus.com/api/oauth/2.0/access_token/";
+		
 	}
 
 	/**
@@ -33,25 +33,25 @@ class Hybrid_Providers_Disqus extends Hybrid_Provider_Model_OAuth2
 	*/
 	function getUserProfile()
 	{
-		$data = $this->api->get( "users/details" ); 
-		print_r($data);
+		$data = $this->api->get( "users/details" , array('api_key' => $this->api->client_id, 'api_secret' => $this->api->client_secret)); 
+		
 		if ( ! isset( $data->code ) ){
 			throw new Exception( "User profile request failed! {$this->providerId} returned an invalid response.", 6 );
 		} else if ( $data->code != 0 ){
 			throw new Exception( "User profile request failed! {$this->providerId} returned error code".$data->code.".", 6 );
 		}
 
-		$this->user->profile->identifier  = @ $data->id; 
-		$this->user->profile->displayName = @ $data->name;
-		$this->user->profile->description = @ $data->bio;
-		$this->user->profile->photoURL    = @ $data->avatar_url;
-		$this->user->profile->profileURL  = @ $data->html_url; 
-		$this->user->profile->email       = @ $data->email;
-		$this->user->profile->webSiteURL  = @ $data->blog;
-		$this->user->profile->region      = @ $data->location;
+		$this->user->profile->identifier  = @ $data->response->id; 
+		$this->user->profile->displayName = @ $data->response->name;
+		$this->user->profile->description = @ $data->response->bio;
+		$this->user->profile->photoURL    = @ $data->response->avatar->permalink;
+		$this->user->profile->profileURL  = @ $data->response->profileUrl; 
+		$this->user->profile->email       = @ $data->response->email;
+		$this->user->profile->region      = @ $data->response->location;
+		$this->user->profile->description = @ $data->response->about;
 
 		if( ! $this->user->profile->displayName ){
-			$this->user->profile->displayName = @ $data->login;
+			$this->user->profile->displayName = @ $data->response->username;
 		}
 
 		return $this->user->profile;
