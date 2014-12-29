7b91f3186e87173dbc357b1714baa3f92e0b257e
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2014-12-29 19:41:40 +0200

    dropbox provider has been moved to the additional-providers scope

--- hybridauth/config.php
+++ hybridauth/config.php
@@ -59,11 +59,6 @@ return
 				"enabled" => true,
 				"keys"    => array ( "id" => "", "secret" => "" ) 
 			),
-
-			"Dropbox" => array (
-				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ) 
-			),
 		),
 
 		// If you want to enable logging, set 'debug_mode' to true.
