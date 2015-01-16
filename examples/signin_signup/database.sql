1a5f883d62aaea6e13c44e79a0c419873883c537
Bimal Poudel <poudel@bimal.org.np>
2015-01-16 20:57:49 +0545

    Default values assigned to the users and authentication tables.

--- examples/signin_signup/database.sql
+++ examples/signin_signup/database.sql
@@ -1,27 +1,27 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `first_name` varchar(200) NOT NULL,
-  `last_name` varchar(200) NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+CREATE TABLE `authentications` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'refer to users.id',
+  `provider` VARCHAR(100) NOT NULL DEFAULT '',
+  `provider_uid` VARCHAR(255) NOT NULL DEFAULT '',
+  `email` VARCHAR(200) NOT NULL DEFAULT '',
+  `display_name` VARCHAR(150) NOT NULL DEFAULT '',
+  `first_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `last_name` VARCHAR(100) NOT NULL DEFAULT '',
+  `profile_url` VARCHAR(300) NOT NULL DEFAULT '',
+  `website_url` VARCHAR(300) NOT NULL DEFAULT '',
+  `created_at` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `provider_uid` (`provider_uid`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `authentications` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL COMMENT 'refer to users.id',
-  `provider` varchar(100) NOT NULL,
-  `provider_uid` varchar(255) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `display_name` varchar(150) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `profile_url` varchar(300) NOT NULL,
-  `website_url` varchar(300) NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `provider_uid` (`provider_uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+CREATE TABLE `users` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(200) NOT NULL DEFAULT '',
+  `password` VARCHAR(200) NOT NULL DEFAULT '',
+  `first_name` VARCHAR(200) NOT NULL DEFAULT '',
+  `last_name` VARCHAR(200) NOT NULL DEFAULT '',
+  `created_at` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
