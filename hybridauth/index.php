bb1ed163ae828017adb5701a21731cf406c081cd
Bimal Poudel <poudel@bimal.org.np>
2015-01-16 21:57:25 +0545

    spl_autoload_register() for including class files.

--- hybridauth/index.php
+++ hybridauth/index.php
@@ -9,7 +9,18 @@
 //	HybridAuth End Point
 // ------------------------------------------------------------------------
 
-require_once( "Hybrid/Auth.php" );
-require_once( "Hybrid/Endpoint.php" ); 
+function _include_class_file($Class_Name='')
+{
+	$class_file = dirname(__FILE__).'/'.str_replace('_', '/', $Class_Name).'.php';
+	if(is_file($class_file))
+	{
+		require_once($class_file);
+	}
+	echo $class_file;
+}
+spl_autoload_register('_include_class_file');
+
+#require_once( "Hybrid/Auth.php" );
+#require_once( "Hybrid/Endpoint.php" ); 
 
 Hybrid_Endpoint::process();
