0bbd3d109bc4d3226d45c7524f2ce154825fce01
Sandro Groganz <groganz@users.noreply.github.com>
2015-02-21 22:34:56 +0000

    Remove license/copyright info

--- additional-providers/hybridauth-mailchimp/Providers/MailChimp.php
+++ additional-providers/hybridauth-mailchimp/Providers/MailChimp.php
@@ -1,13 +1,4 @@
 <?php
-/*
- * This file is part of the CampaignChain package.
- *
- * (c) Sandro Groganz <sandro@campaignchain.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /**
  * Hybrid_Providers_MailChimp
  *
