425eda327b7438910c9423b831e8c09f57446e83
AdwinTrave <jxd3455@rit.edu>
2015-03-26 19:51:55 -0400

    Year and version bump
    Also revert index.php code from #381

--- hybridauth/config.php
+++ hybridauth/config.php
@@ -2,69 +2,69 @@
 /**
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2014, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+* (c) 2009-2015, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
 */
 
 // ----------------------------------------------------------------------------------------
 //	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
 // ----------------------------------------------------------------------------------------
 
-return 
+return
 	array(
-		"base_url" => "http://localhost/hybridauth-git/hybridauth/", 
+		"base_url" => "http://localhost/hybridauth-git/hybridauth/",
 
-		"providers" => array ( 
+		"providers" => array (
 			// openid providers
 			"OpenID" => array (
 				"enabled" => true
 			),
 
-			"Yahoo" => array ( 
+			"Yahoo" => array (
 				"enabled" => true,
 				"keys"    => array ( "key" => "", "secret" => "" ),
 			),
 
-			"AOL"  => array ( 
-				"enabled" => true 
+			"AOL"  => array (
+				"enabled" => true
 			),
 
-			"Google" => array ( 
+			"Google" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ), 
+				"keys"    => array ( "id" => "", "secret" => "" ),
 			),
 
-			"Facebook" => array ( 
+			"Facebook" => array (
 				"enabled" => true,
 				"keys"    => array ( "id" => "", "secret" => "" ),
 				"trustForwarded" => false
 			),
 
-			"Twitter" => array ( 
+			"Twitter" => array (
 				"enabled" => true,
-				"keys"    => array ( "key" => "", "secret" => "" ) 
+				"keys"    => array ( "key" => "", "secret" => "" )
 			),
 
 			// windows live
-			"Live" => array ( 
+			"Live" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ) 
+				"keys"    => array ( "id" => "", "secret" => "" )
 			),
 
-			"LinkedIn" => array ( 
+			"LinkedIn" => array (
 				"enabled" => true,
-				"keys"    => array ( "key" => "", "secret" => "" ) 
+				"keys"    => array ( "key" => "", "secret" => "" )
 			),
 
 			"Foursquare" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "", "secret" => "" ) 
+				"keys"    => array ( "id" => "", "secret" => "" )
 			),
 		),
 
 		// If you want to enable logging, set 'debug_mode' to true.
 		// You can also set it to
 		// - "error" To log only error messages. Useful in production
-		// - "info" To log info and error messages (ignore debug messages) 
+		// - "info" To log info and error messages (ignore debug messages)
 		"debug_mode" => false,
 
 		// Path to file writable by the web server. Required if 'debug_mode' is not false
--- hybridauth/index.php
+++ hybridauth/index.php
@@ -2,25 +2,14 @@
 /**
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2014, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html 
+* (c) 2009-2015, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
 */
 
 // ------------------------------------------------------------------------
 //	HybridAuth End Point
 // ------------------------------------------------------------------------
 
-function _include_class_file($Class_Name='')
-{
-	$class_file = dirname(__FILE__).'/'.str_replace('_', '/', $Class_Name).'.php';
-	if(is_file($class_file))
-	{
-		require_once($class_file);
-	}
-	#echo $class_file;
-}
-spl_autoload_register('_include_class_file');
-
-#require_once( "Hybrid/Auth.php" );
-#require_once( "Hybrid/Endpoint.php" ); 
+require_once( "Hybrid/Auth.php" );
+require_once( "Hybrid/Endpoint.php" );
 
 Hybrid_Endpoint::process();
