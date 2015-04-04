hybridauth/hybridauth

commit bf9fb5efb9ac770ded293f799ca4063fb8369df4

Add class property for responses to be stored

hybridauth/Hybrid/thirdparty/OAuth/OAuth2Client.php
@@ -34,12 +34,13 @@ class OAuth2Client
 	public $curl_header              = array();
 	public $curl_useragent           = "OAuth/2 Simple PHP Client v0.1; HybridAuth http://hybridauth.sourceforge.net/";
 	public $curl_authenticate_method = "POST";
-        public $curl_proxy               = null;
+	public $curl_proxy               = null;
 
 	//--
 
 	public $http_code             = "";
 	public $http_info             = "";
+	protected $response           = null;
 
 	//--
 
@@ -140,10 +141,20 @@ class OAuth2Client
 		}
 
 		if( $response && $this->decode_json ){
-			$response = json_decode( $response );
+			$this->response = json_decode( $response );
 		}
 
-		return $response;
+		return $this->response = $response;
+	}
+
+	/**
+	 * Return the response object afer the fact
+	 *
+	 * @return mixed
+	 */
+	public function getResponse()
+	{
+	    return $this->response;
 	}
 
 	/**

hybridauth/Hybrid/thirdparty/Facebook/base_facebook.php
@@ -171,6 +171,13 @@ abstract class BaseFacebook
   );
 
   /**
+   * The decoded response object.
+   *
+   * @var mixed
+   */
+  protected $response;
+
+  /**
    * The Application ID.
    *
    * @var string
@@ -449,6 +456,16 @@ abstract class BaseFacebook
     }
 
     return $this->accessToken;
+  }
+
+  /**
+   * Return the response object afer the fact
+   *
+   * @return mixed
+   */
+  public function getResponse()
+  {
+      return $this->response;
   }
 
   /**
@@ -913,7 +930,7 @@ abstract class BaseFacebook
     }
     // @codeCoverageIgnoreEnd
 
-    return $result;
+    return $this->response = $result;
   }
 
   /**

hybridauth/Hybrid/thirdparty/OAuth/OAuth1Client.php
@@ -31,6 +31,7 @@ class OAuth1Client{
 
 	public $http_code             = "";
 	public $http_info             = "";
+	protected $response           = null;
 
 	/**
 	* OAuth client constructor
@@ -138,7 +139,17 @@ class OAuth1Client{
 			$response = json_decode( $response );
 		}
 
-		return $response;
+		return $this->response = $response;
+	}
+
+	/**
+	 * Return the response object afer the fact
+	 *
+	 * @return mixed
+	 */
+	public function getResponse()
+	{
+	    return $this->response;
 	}
 
 	/**

commit 8f9e697da0be55125b732f3f973b17e3ae3e3607

Fix for serious bug introduced in bf9fb5efb9ac770ded293f799ca4063fb8369df4. Decoded JSON data before return were replaced back by encoded, in particular, Vkontakte failed to work because of this (Facebook worked anyway though).

hybridauth/Hybrid/thirdparty/OAuth/OAuth2Client.php
@@ -144,7 +144,7 @@ class OAuth2Client
 			$this->response = json_decode( $response );
 		}
 
-		return $this->response = $response;
+		return $this->response;
 	}
 
 	/**