255d74a05c91df52ad3eca2b28134d4113555ab4
trogwar <hroftt@gmail.com>
2015-05-24 17:52:40 +0300

    Make able to set fields of user profile from HybridAuth config

--- additional-providers/hybridauth-vkontakte/Providers/Vkontakte.php
+++ additional-providers/hybridauth-vkontakte/Providers/Vkontakte.php
@@ -18,9 +18,29 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 	// default permissions
 	public $scope = "email";
 
+	// default user fields map
+	public $fields = array(
+		'identifier'  => 'uid',
+		'firstName'   => 'first_name',
+		'lastName'    => 'last_name',
+		'gender'      => 'sex',
+		'displayName' => 'screen_name',
+		'city'        => 'city',
+		'country'     => 'country',
+		'bdate'       => 'bdate',
+		'photoURL'    => 'photo_max_orig',
+		'nickname'    => 'nickname',
+		'domain'      => 'domain',
+		'timezone'    => 'timezone',
+		'photo_rec'   => 'photo_rec',
+		'photo_max'   => 'photo_max',
+		'home_phone'  => 'home_phone',
+		'home_town'   => 'home_town',
+	);
+
 	/**
-	* IDp wrappers initializer
-	*/
+	 * IDp wrappers initializer
+	 */
 	function initialize()
 	{
 		parent::initialize();
@@ -29,7 +49,8 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 		$this->api->api_base_url   = 'https://api.vk.com/method/';
 		$this->api->authorize_url  = "https://api.vk.com/oauth/authorize";
 		$this->api->token_url      = "https://api.vk.com/oauth/token";
-		//$this->api->token_info_url
+		if (!empty($this->config['fields']))
+			$this->fields = $this->config['fields'];
 	}
 
 	function loginFinish()
@@ -71,8 +92,8 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 	}
 
 	/**
-	* load the user profile from the IDp api client
-	*/
+	 * load the user profile from the IDp api client
+	 */
 	function getUserProfile()
 	{
 		// refresh tokens if needed
@@ -80,7 +101,7 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 
 		// Vkontakte requires user id, not just token for api access
 		$params['uid'] = Hybrid_Auth::storage()->get( "hauth_session.{$this->providerId}.user_id" );
-		$params['fields'] = 'first_name,last_name,nickname,screen_name,sex,bdate,timezone,photo_rec,photo_big,home_town';
+		$params['fields'] = implode(',', $this->fields);
 		// ask vkontakte api for user infos
 
 		$response = $this->api->api( 'getProfiles' , 'GET', $params);
@@ -89,71 +110,102 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 			throw new Exception( "User profile request failed! {$this->providerId} returned an invalid response.", 6 );
 		}
 
-		$response = $response->response[0];
-		$this->user->profile->identifier    = (property_exists($response,'uid'))?$response->uid:"";
-		$this->user->profile->firstName     = (property_exists($response,'first_name'))?$response->first_name:"";
-		$this->user->profile->lastName      = (property_exists($response,'last_name'))?$response->last_name:"";
-		$this->user->profile->displayName   = (property_exists($response,'screen_name'))?$response->screen_name:"";
-		$this->user->profile->photoURL      = (property_exists($response,'photo_big'))?$response->photo_big:"";
-		$this->user->profile->profileURL    = (property_exists($response,'screen_name'))?"http://vk.com/" . $response->screen_name:"";
-		$this->user->profile->email         = Hybrid_Auth::storage()->get( "hauth_session.{$this->providerId}.user_email" );
-		$this->user->profile->home_town     = (property_exists($response,'home_town'))?$response->home_town:"";
+		// Fill datas
+		$response = reset($response->response);
+		foreach ($this->getUserByResponse($response, true) as $k => $v)
+			$this->user->profile->$k = $v;
 
-		if(property_exists($response,'sex')){
-			switch ($response->sex)
-			{
-				case 1: $this->user->profile->gender = 'female'; break;
-				case 2: $this->user->profile->gender = 'male'; break;
-				default: $this->user->profile->gender = ''; break;
-		$response = $this->api->api('friends.get','GET',$params);
-			}
-		}
-
-		if( property_exists($response,'bdate') ){
-
-			$birthday = explode('.', $response->bdate);
-
-			switch (count($birthday)) {
-				case 3:
-					$this->user->profile->birthDay   = (int) $birthday[0];
-					$this->user->profile->birthMonth = (int) $birthday[1];
-					$this->user->profile->birthYear  = (int) $birthday[2];
-					break;
-
-				case 2:
-					$this->user->profile->birthDay   = (int) $birthday[0];
-					$this->user->profile->birthMonth = (int) $birthday[1];
-					break;
-			}
-		}
+		// Additional data
+		$this->user->profile->email = Hybrid_Auth::storage()->get( "hauth_session.{$this->providerId}.user_email" );
 
 		return $this->user->profile;
 	}
 
 	/**
-	* load the user contacts
-	*/
+	 * load the user contacts
+	 */
 	function getUserContacts()
 	{
 		$params=array(
-			'fields' => 'nickname, domain, sex, bdate, city, country, timezone, photo_200_orig'
+			'fields' => implode(',', $this->fields),
 		);
 
+		$response = $this->api->api('friends.get','GET',$params);
 
-		if(!$response || !count($response->response)){
+		if(empty($response) || empty($response->response)){
 			return array();
 		}
 
 		$contacts = array();
-		foreach( $response->response as $item ){
-			$uc = new Hybrid_User_Contact();
-			$uc->identifier  = $item->uid;
-			$uc->displayName = $item->first_name.' '.$item->last_name;
-			$uc->profileURL  = 'http://vk.com/'.$item->domain;
-			$uc->photoURL    = $item->photo_200_orig;
-			$contacts[] = $uc;
+		foreach( $response->response as $item ) {
+			$contacts[] = $this->getUserByResponse($item);
 		}
 
 		return $contacts;
+	}
+
+	/**
+	 * @param object $response
+	 * @param bool   $withAdditionalRequests True to get some full fields like 'city' or 'country'
+	 *                                       (requires additional responses to vk api!)
+	 *
+	 * @return \Hybrid_User_Contact
+	 */
+	function getUserByResponse($response, $withAdditionalRequests = false)
+	{
+		$user = new Hybrid_User_Contact();
+
+		foreach ($this->fields as $field => $map)
+			$user->$field = (property_exists($response,$map)) ? $response->$map : null;
+
+		if (property_exists($user, 'profileURL') && !empty($user->profileURL)) {
+			$user->profileURL = 'http://vk.com/' . $user->profileURL;
+		}
+
+		if (property_exists($user, 'gender')) {
+			switch ($user->gender) {
+				case 1: $user->gender = 'female'; break;
+				case 2: $user->gender = 'male'; break;
+				default: $user->gender = null; break;
+			}
+		}
+
+		if (property_exists($user, 'bdate')) {
+			$birthday = explode('.', $user->bdate);
+			switch (count($birthday)) {
+				case 3:
+					$user->birthDay   = (int) $birthday[0];
+					$user->birthMonth = (int) $birthday[1];
+					$user->birthYear  = (int) $birthday[2];
+					break;
+
+				case 2:
+					$user->birthDay   = (int) $birthday[0];
+					$user->birthMonth = (int) $birthday[1];
+					break;
+			}
+		}
+
+		if (property_exists($user, 'city') && $withAdditionalRequests) {
+			$params     = array(
+				'city_ids' => $user->city,
+			);
+			$cities     = $this->api->api( 'database.getCitiesById' , 'GET', $params);
+			$city       = reset($cities);
+			if (is_array($city)) $city = reset($city);
+			$user->city = property_exists($city, 'name') ? $city->name : null;
+		}
+
+		if (property_exists($user, 'country') && $withAdditionalRequests) {
+			$params        = array(
+				'country_ids' => $user->country,
+			);
+			$countries     = $this->api->api( 'database.getCountriesById' , 'GET', $params);
+			$country       = reset($countries);
+			if (is_array($country)) $country = reset($country);
+			$user->country = property_exists($country, 'name') ? $country->name : null;
+		}
+
+		return $user;
 	}
 }
