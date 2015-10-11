b4f0c46fd4050568d37da2671404cba97152cef9
Uriy Efremochkin <333peps@gmail.com>
2015-10-11 18:07:38 +0500

    Fix for undefined City and Country.

--- additional-providers/hybridauth-vkontakte/Providers/Vkontakte.php
+++ additional-providers/hybridauth-vkontakte/Providers/Vkontakte.php
@@ -201,7 +201,7 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 			$cities     = $this->api->api( 'database.getCitiesById' , 'GET', $params);
 			$city       = reset($cities);
 			if (is_array($city)) $city = reset($city);
-			$user->city = property_exists($city, 'name') ? $city->name : null;
+			$user->city = is_object($city) && property_exists($city, 'name') ? $city->name : null;
 		}
 
 		if (property_exists($user, 'country') && $withAdditionalRequests) {
@@ -211,7 +211,7 @@ class Hybrid_Providers_Vkontakte extends Hybrid_Provider_Model_OAuth2
 			$countries     = $this->api->api( 'database.getCountriesById' , 'GET', $params);
 			$country       = reset($countries);
 			if (is_array($country)) $country = reset($country);
-			$user->country = property_exists($country, 'name') ? $country->name : null;
+			$user->country = is_object($country) && property_exists($country, 'name') ? $country->name : null;
 		}
 
 		return $user;
