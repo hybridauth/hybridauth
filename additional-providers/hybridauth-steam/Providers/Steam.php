5f13ea332f23a7dcdf8d24c718f0a4a0f068ec65
Alan Bondarchuk <fortis@users.noreply.github.com>
2015-10-29 12:22:05 +0500

    Fix profileUrl
    Need string conversion for $data->customURL and check it is not empty

--- additional-providers/hybridauth-steam/Providers/Steam.php
+++ additional-providers/hybridauth-steam/Providers/Steam.php
@@ -72,6 +72,12 @@ class Hybrid_Providers_Steam extends Hybrid_Provider_Model_OpenID
 
 		# store the user profile.  
 		//$this->user->profile->identifier		=	"";
+		if (property_exists($data, 'customURL') && (string) $data->customURL != '') {
+      			$this->user->profile->profileURL = 'http://steamcommunity.com/id/' . (string) $data->customURL . '/';
+    		}
+    		else {
+      			$this->user->profile->profileURL = "http://steamcommunity.com/profiles/{$this->user->profile->identifier}/";
+    		}
 		$this->user->profile->profileURL		=	property_exists($data, 'customURL') ? "http://steamcommunity.com/id/{$data->customURL}/" : "http://steamcommunity.com/profiles/{$this->user->profile->identifier}/";
 		$this->user->profile->webSiteURL		=	"";
 		$this->user->profile->photoURL			=	property_exists($data, 'avatarFull') ? (string)$data->avatarFull : '';
