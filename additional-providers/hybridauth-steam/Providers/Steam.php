304a7d92f839a9eeef3040efce6fc6594070f65f
Alan Bondarchuk <fortis@users.noreply.github.com>
2015-10-29 12:28:58 +0500

    Remove old line of code

--- additional-providers/hybridauth-steam/Providers/Steam.php
+++ additional-providers/hybridauth-steam/Providers/Steam.php
@@ -78,7 +78,7 @@ class Hybrid_Providers_Steam extends Hybrid_Provider_Model_OpenID
     		else {
       			$this->user->profile->profileURL = "http://steamcommunity.com/profiles/{$this->user->profile->identifier}/";
     		}
-		$this->user->profile->profileURL		=	property_exists($data, 'customURL') ? "http://steamcommunity.com/id/{$data->customURL}/" : "http://steamcommunity.com/profiles/{$this->user->profile->identifier}/";
+		
 		$this->user->profile->webSiteURL		=	"";
 		$this->user->profile->photoURL			=	property_exists($data, 'avatarFull') ? (string)$data->avatarFull : '';
 		$this->user->profile->displayName		=	property_exists($data, 'steamID') ? (string)$data->steamID : '';
