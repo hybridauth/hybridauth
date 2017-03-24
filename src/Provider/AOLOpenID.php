ad6def7d78580fcd08bde2438b4407fa8d7941b7
Miled <miled@users.noreply.github.com>
2017-03-24 17:40:11 +0100

    Rename file

---
+++ src/Provider/AOLOpenID.php
@@ -0,0 +1,21 @@
+<?php
+/*!
+* Hybridauth
+* https://hybridauth.github.io | https://github.com/hybridauth/hybridauth
+*  (c) 2017 Hybridauth authors | https://hybridauth.github.io/license.html
+*/
+
+namespace Hybridauth\Provider;
+
+use Hybridauth\Adapter\OpenID;
+
+/**
+ * AOL OpenID provider adapter.
+ */
+class AOLOpenID extends OpenID
+{
+    /**
+    * {@inheritdoc}
+    */
+    protected $openidIdentifier = 'http://openid.aol.com/';
+}
