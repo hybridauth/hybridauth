3d98822fcd2656085504afe94acc6c5e5302cb6a
Miled <miled@users.noreply.github.com>
2017-03-24 17:43:42 +0100

    Add usage example

--- src/Provider/OpenID.php
+++ src/Provider/OpenID.php
@@ -9,6 +9,37 @@ namespace Hybridauth\Provider;
 
 use Hybridauth\Adapter;
 
+/**
+ * Generic OpenID providers adapter.
+ *
+ * Example:
+ *
+ *   $config = [
+ *       'callback' => Hybridauth\HttpClient\Util::getCurrentUrl(),
+ *
+ *       //  authenticate with Yahoo openid
+ *       'openid_identifier' => 'https://open.login.yahooapis.com/openid20/www.yahoo.com/xrds' 
+ *
+ *       //  authenticate with stackexchange network openid
+ *       // 'openid_identifier' => 'https://openid.stackexchange.com/',
+ *
+ *       //  authenticate with Steam openid 
+ *       // 'openid_identifier' => 'http://steamcommunity.com/openid',
+ *
+ *       // etc.
+ *   ];
+ *
+ *   $adapter = new Hybridauth\Provider\OpenID( $config );
+ *
+ *   try {
+ *       $adapter->authenticate();
+ *
+ *       $userProfile = $adapter->getUserProfile();
+ *   }
+ *   catch( Exception $e ){
+ *       echo $e->getMessage() ;
+ *   }
+ */
 class OpenID extends Adapter\OpenID
 {
 }
