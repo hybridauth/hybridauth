76e4986a8af10307bc4ca92ddcc37bb152414745
Miled <miled@users.noreply.github.com>
2017-03-30 23:21:39 +0200

    Fix getUserProfile

--- src/Provider/Instagram.php
+++ src/Provider/Instagram.php
@@ -40,19 +40,24 @@ class Instagram extends OAuth2
     /**
     * {@inheritdoc}
     */
+    protected $apiDocumentation = 'https://www.instagram.com/developer/authentication/';
+
+    /**
+    * {@inheritdoc}
+    */
     public function getUserProfile()
     {
         $response = $this->apiRequest('users/self/');
 
         $data = new Data\Collection($response);
 
-        if (! $data->exists('id')) {
+        if (! $data->exists('data')) {
             throw new UnexpectedValueException('Provider API returned an unexpected response.');
         }
 
         $userProfile = new User\Profile();
 
-        $data = $data->filter('id');
+        $data = $data->filter('data');
 
         $userProfile->identifier  = $data->get('id');
         $userProfile->description = $data->get('bio');
