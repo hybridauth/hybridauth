9f404fdda6d5529e0fb2859e2a9aa8f5b4423f99
Miled <miled@users.noreply.github.com>
2017-04-07 20:26:42 +0200

    Implement new methods to mangage connected adapters

--- src/Hybridauth.php
+++ src/Hybridauth.php
@@ -19,8 +19,12 @@ use Hybridauth\HttpClient\Curl as HttpClient;
 use Hybridauth\Deprecated\DeprecatedHybridauthTrait;
 
 /**
- * The sole purpose for this class is to provide an unified entry point for the various providers
- * and to ensure a MINIMAL backward compatibility with Hybridauth 2.x.
+ * Hybridauth\Hybridauth
+ *
+ * For ease of use of multiple providers, Hybridauth implements the class Hybridauth\Hybridauth, 
+ * a sort of factory/façade which acts as an unified interface or entry point, and it expects a
+ * configuration array containing the list of providers you want to use, their respective credentials
+ * and authorized callback.
  */
 class Hybridauth
 {
@@ -53,7 +57,7 @@ class Hybridauth
     protected $logger;
 
     /**
-    * @param array|string        $config     Array with configuration or path to PHP file that will return array
+    * @param array|string        $config     Array with configuration or Path to PHP file that will return array
     * @param HttpClientInterface $httpClient
     * @param StorageInterface    $storage
     * @param LoggerInterface     $logger
@@ -63,20 +67,21 @@ class Hybridauth
     public function __construct(
         $config = [],
         HttpClientInterface $httpClient = null,
-        StorageInterface    $storage = null,
-        LoggerInterface     $logger = null
+        StorageInterface $storage = null,
+        LoggerInterface $logger = null
     ) {
         if (is_string($config) && file_exists($config)) {
             $config = include $config;
-        } elseif (! is_array($config)) {
+        }
+        elseif (! is_array($config)) {
             throw new InvalidArgumentException('Hybriauth config does not exist on the given path.');
         }
 
         $this->config = $config + [
-            'debug_mode' => Logger::NONE,
-            'debug_file' => '',
+            'debug_mode'   => Logger::NONE,
+            'debug_file'   => '',
             'curl_options' => null,
-            'providers' => []
+            'providers'    => []
         ];
 
         $this->storage = $storage ?: new Session();
@@ -100,29 +105,15 @@ class Hybridauth
     * If user not authenticated yet, the user will be redirected to the authorization Service
     * to authorize the application.
     *
-    * @param string $provider Provider (case insensitive)
-    *
-    * @throws Exception\Exception
-    * @throws Exception\RuntimeException
-    * @throws Exception\UnexpectedValueException
-    * @throws Exception\InvalidArgumentException
-    * @throws Exception\AuthorizationDeniedException
-    * @throws Exception\HttpClientFailureException
-    * @throws Exception\HttpRequestFailedException
-    * @throws Exception\InvalidAccessTokenException
-    * @throws Exception\InvalidApplicationCredentialsException
-    * @throws Exception\InvalidAuthorizationCodeException
-    * @throws Exception\InvalidAuthorizationStateException
-    * @throws Exception\InvalidOauthTokenException
-    * @throws Exception\InvalidOpenidIdentifierException
+    * @param string $name adapter's name (case insensitive)
     *
     * @return Adapter\AdapterInterface
     */
-    public function authenticate($provider)
+    public function authenticate($name)
     {
-        $this->logger->info("Enter Hybridauth::authenticate( $provider )");
+        $this->logger->info(sprintf('Hybridauth::authenticate(%s)', $name));
 
-        $adapter = $this->getAdapter($provider);
+        $adapter = $this->getAdapter($name);
 
         $adapter->authenticate();
 
@@ -130,20 +121,17 @@ class Hybridauth
     }
 
     /**
-    * Instantiate and returns the given provider adapter.
+    * Returns a new instance of a provider's adapter by name
     *
-    * @param string $provider Provider (case insensitive)
-    *
-    * @throws Exception\UnexpectedValueException
-    * @throws Exception\InvalidArgumentException
+    * @param string $name adapter's name (case insensitive)
     *
     * @return Adapter\AdapterInterface
     */
-    public function getAdapter($provider)
+    public function getAdapter($name)
     {
-        $config = $this->getProviderConfig($provider);
+        $config = $this->getProviderConfig($name);
 
-        $adapter = "Hybridauth\\Provider\\$provider";
+        $adapter = "Hybridauth\\Provider\\$name";
 
         return new $adapter($config, $this->httpClient, $this->storage, $this->logger);
     }
@@ -151,16 +139,16 @@ class Hybridauth
     /**
     * Get provider config by name.
     *
-    * @param string $provider Provider (case insensitive)
+    * @param string $name adapter's name (case insensitive)
     *
     * @throws Exception\UnexpectedValueException
     * @throws Exception\InvalidArgumentException
     *
     * @return array
     */
-    protected function getProviderConfig($provider)
+    protected function getProviderConfig($name)
     {
-        $provider = strtolower($provider);
+        $name = strtolower($name);
 
         $providersConfig = array_change_key_case($this->config['providers'], CASE_LOWER);
 
@@ -168,16 +156,71 @@ class Hybridauth
             throw new InvalidArgumentException('Unknown Provider.');
         }
 
-        if (! $providersConfig[$provider]['enabled']) {
+        if (! $providersConfig[$name]['enabled']) {
             throw new UnexpectedValueException('Disabled Provider.');
         }
 
-        $config = $providersConfig[$provider];
+        $config = $providersConfig[$name];
 
         if (! isset($config['callback']) && isset($this->config['callback'])) {
             $config['callback'] = $this->config['callback'];
         }
 
         return $config;
+    }
+
+    /**
+    * Returns a boolean of whether the user is connected with a provider 
+    *
+    * @param string $name adapter's name (case insensitive)
+    *
+    * @return boolean 
+    */
+    public static function isConnectedWith($name)
+    {
+        return $this->getAdapter($name)->isConnected();
+    }
+
+    /**
+    * Returns a list of currently connected adapters names
+    *
+    * @return array
+    */
+    public static function getConnectedProviders() {
+        $providers = [];
+
+        foreach ($this->config['providers'] as $name => $params) {
+            if ($this->isConnectedWith($name)) {
+                $providers[] = $name;
+            }
+        }
+
+        return $providers;
+    }
+
+    /**
+    * Returns a list of new instances of currently connected adapters
+    *
+    * @return array
+    */
+    public static function getConnectedAdapters() {
+        $adapters = [];
+
+        foreach ($this->config['providers'] as $name => $params) {
+            if ($this->isConnectedWith($name)) {
+                $adapters[$name] = $this->getAdapter($name);
+            }
+        }
+
+        return $adapters;
+    }
+
+    /**
+    * Disconnect all currently connected adapters at once
+    */
+    public static function disconnectAllAdapters()
+    {
+        // we simply clear stored data
+        $this->storage->clear();
     }
 }
