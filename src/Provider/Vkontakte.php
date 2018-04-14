2f849edcfb47b756ec08483f2d7eb1ed2e91a9bb
David Pashaev <pashaev.d@i20.biz>
2018-04-14 23:50:33 +0700

    #771: Revert old code.

--- src/Provider/Vkontakte.php
+++ src/Provider/Vkontakte.php
@@ -1,4 +1,9 @@
 <?php
+/*!
+* Hybridauth
+* https://hybridauth.github.io | https://github.com/hybridauth/hybridauth
+*  (c) 2017 Hybridauth authors | https://hybridauth.github.io/license.html
+*/
 
 namespace Hybridauth\Provider;
 
