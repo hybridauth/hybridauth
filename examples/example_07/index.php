fa862ade759959cb852110f3199d2a5ca69d8a40
neokio <neokio@gmail.com>
2018-04-24 11:35:59 -0400

    A bit of cleanup and rewrite to existing coding standards

--- examples/example_07/index.php
+++ examples/example_07/index.php
@@ -17,57 +17,45 @@ $adapters = $hybridauth->getConnectedAdapters();
 <head>
     <meta charset="UTF-8">
     <title>Example 07</title>
+
+    <script>
+        function auth_popup( provider ){
+            // replace 'path/to/hybridauth' with the real path to this script
+            var authWindow = window.open('https://path/to/hybridauth/examples/example_07/callback.php?provider='+provider, 'authWindow', 'width=600,height=400,scrollbars=yes');
+            return false;
+        }
+    </script>
+    
 </head>
 <body>
     <h1>Sign in</h1>
 
     <ul>
 
-<?php
-    foreach ($hybridauth->getProviders() as $name) {
-        if (!isset($adapters[$name])) {
-            echo '
+<?php foreach ($hybridauth->getProviders() as $name) : ?>
+    <?php if (!isset($adapters[$name])) : ?>
         <li>
-            <a href="#" onclick="javascript:auth_popup(\''. $name .'\');">
-                Sign in with '. $name .'
+            <a href="#" onclick="javascript:auth_popup('<?php print $name ?>');">
+                Sign in with <?php print $name ?>
             </a>
-        </li>';
-        }
-    }
-?>
+        </li>
+    <?php endif; ?>
+<?php endforeach; ?>
 
     </ul>
 
-<?php if ($adapters) { ?>
-    <h1>You are logged in with:</h1>
+<?php if ($adapters) : ?>
+    <h1>You are logged in:</h1>
     <ul>
-<?php
-        foreach ($adapters as $name=>$adapter) {
-            echo '
-        <li>
-            '. $adapter->getUserProfile()->displayName .' from
-            <i>'. $name .'</i>
-            (<a href="'. $config['callback'] .'?logout='. $name .'">Log Out</a>)
-        </li>';
-        }
-?>
+        <?php foreach ($adapters as $name => $adapter) : ?>
+            <li>
+                <strong><?php print $adapter->getUserProfile()->displayName; ?></strong> from
+                <i><?php print $name; ?></i>
+                <span>(<a href="<?php print $config['callback'] . "?logout={$name}"; ?>">Log Out</a>)</span>
+            </li>
+        <?php endforeach; ?>
     </ul>
-<?php } ?>
+<?php endif; ?>
 
 </body>
 </html>
-
-
-
-
-<a href="#" onclick="javascript:auth_popup('Google');">Sign in with Google</a>
-<a href="#" onclick="javascript:auth_popup('Facebook');">Sign in with Facebook</a>
-
-
-<script>
-    function auth_popup( provider ){
-        // replace 'path/to/hybridauth' with the real path to this script
-        var authWindow = window.open('https://path/to/hybridauth/examples/example_07/callback.php?provider='+provider, 'authWindow', 'width=600,height=400,scrollbars=yes');
-        return false;
-    }
-</script>
