5e22dbb9bd1e8e07ed1ce1b9d2ca090e5954d901
Nitipat L <iamsvz@gmail.com>
2018-09-03 22:20:07 +0700

    [Edited] add current year and my name

--- src/Provider/Line.php
+++ src/Provider/Line.php
@@ -2,7 +2,7 @@
 /*!
 * Hybridauth
 * https://hybridauth.github.io | https://github.com/hybridauth/hybridauth
-*  (c) 2017 Hybridauth authors | https://hybridauth.github.io/license.html
+*  (c) 2018 Nitipat Lowichakornthikun | https://hybridauth.github.io/license.html
 */
 namespace Hybridauth\Provider;
 
