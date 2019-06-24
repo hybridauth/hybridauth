54eafd08f799c9666288954d205b5e1eb724bd3b
Vincent Privat <vincent.privat@gmail.com>
2019-06-24 20:05:16 +0200

    [OSM] Migrate OpenStreetMap provider to v3

---
+++ src/Provider/OpenStreetMap.php
@@ -0,0 +1,64 @@
+<?php
+/*!
+* HybridAuth
+* https://hybridauth.github.io | https://github.com/hybridauth/hybridauth
+*  (c) 2017 Hybridauth authors | https://hybridauth.github.io/license.html
+*/
+
+namespace Hybridauth\Provider;
+
+use Hybridauth\Adapter\OAuth1;
+use Hybridauth\Exception\UnexpectedApiResponseException;
+use Hybridauth\Data;
+use Hybridauth\User;
+
+/**
+ * OpenStreetMap OAuth1 provider adapter.
+ */
+class OpenStreetMap extends OAuth1
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $apiBaseUrl = 'https://api.openstreetmap.org/api/0.6/';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $authorizeUrl = 'https://www.openstreetmap.org/oauth/authorize';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $requestTokenUrl = 'https://www.openstreetmap.org/oauth/request_token';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $accessTokenUrl = 'https://www.openstreetmap.org/oauth/access_token';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $apiDocumentation = 'https://wiki.openstreetmap.org/wiki/OAuth';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserProfile()
+    {
+        $response = $this->apiRequest('user/details');
+
+        $response = new \SimpleXMLElement($response);
+
+        $userProfile = new User\Profile();
+
+        $userProfile->identifier    = (string) $response->user['id'];
+        $userProfile->displayName   = (string) $response->user['display_name'];
+        $userProfile->description   = (string) $response->user->description;
+        $userProfile->photoURL      = (string) $response->user->img['href'];
+        $userProfile->profileURL    = 'https://www.openstreetmap.org/user/' . $userProfile->displayName;
+
+        return $userProfile;
+    }
+}
