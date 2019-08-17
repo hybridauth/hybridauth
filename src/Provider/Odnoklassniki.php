6051289a5c6a1338c2b8315afd8742112fc06c4f
Anton <info@ensostudio.ru>
2019-08-17 08:49:56 +0300

    Update Odnoklassniki.php
    1. Add scopes, see https://apiok.ru/en/ext/oauth/permissions
    2. Separate method for calculate signature + fix: sig value is changed to the lower case
    3. `getUserProfile`: optimize request  + `User\Profile`: set more info

--- src/Provider/Odnoklassniki.php
+++ src/Provider/Odnoklassniki.php
@@ -13,6 +13,7 @@ use Hybridauth\Exception\UnexpectedApiResponseException;
 use Hybridauth\User;
 /**
  * Odnoklassniki OAuth2 provider adapter.
+ * @see https://apiok.ru/en/
  *
  * Example:
  *
@@ -20,22 +21,23 @@ use Hybridauth\User;
  *       'callback'  => Hybridauth\HttpClient\Util::getCurrentUrl(),
  *       'keys'      => ['id' => '', 'key' => '', 'secret' => ''],
  *   ];
-
  *   $adapter = new Hybridauth\Provider\Odnoklassniki($config);
- *
  *   try {
  *       if (!$adapter->isConnected()) {
  *           $adapter->authenticate();
  *       }
- *
  *       $userProfile = $adapter->getUserProfile();
- *   }
- *   catch(\Exception $e) {
- *       print $e->getMessage() ;
+ *   } catch (\Exception $e) {
+ *       print $e->getMessage();
  *   }
  */
 class Odnoklassniki extends OAuth2
 {
+    /**
+    * {@inheritdoc}
+    */
+    protected $scope = 'VALUABLE_ACCESS;LONG_ACCESS_TOKEN;GET_EMAIL';
+    
     /**
     * {@inheritdoc}
     */
@@ -52,30 +54,47 @@ class Odnoklassniki extends OAuth2
     protected $accessTokenUrl = 'https://api.ok.ru/oauth/token.do';
 
     /**
+     * Calculate request signature (sig).
+     * 
+     * @param array $parameters Request data
+     * @return string
+     * @see https://apiok.ru/en/dev/methods/
+     */
+    protected function calculateSignature(array $parameters)
+    {
+        // Take access_token away from the list of parameters, if applicable
+        unset($parameters['access_token']);
+        // Parameters are sorted lexicographically by keys
+        ksort($parameters);
+        // Parameters are joined in the format key=value
+        foreach ($parameters as $key => $value) {
+            $parameters[$key] = $key . '=' . $value;
+        }
+        
+        $sessionSecretKey = md5($this->getStoredData('access_token') . $this->config->get('keys')['secret']);
+
+        $sig = md5(implode($parameters) . strtolower($sessionSecretKey));
+        // The sig value is changed to the lower case
+        return strtolower($sig);
+    }
+    
+    /**
     * {@inheritdoc}
+    * @see https://apiok.ru/en/dev/methods/rest/users/users.getCurrentUser
     */
     public function getUserProfile()
     {
-        $fields = array(
-            'uid', 'locale', 'first_name', 'last_name', 'name', 'gender', 'age', 'birthday',
-            'has_email', 'current_status', 'current_status_id', 'current_status_date','online',
-            'photo_id', 'pic_1', 'pic_2', 'pic1024x768', 'location', 'email'
-        );
-
-        $sig = md5(
-            'application_key=' . $this->config->get('keys')['key'] .
-            'fields=' . implode(',', $fields) .
-            'method=users.getCurrentUser' .
-            md5($this->getStoredData('access_token') . $this->config->get('keys')['secret'])
-        );
-
+        $fields = [
+            'uid', 'url_profile', 'first_name', 'last_name', 'name', 'gender', 
+            'age', 'birthday', 'pic1024x768', 'location', 'locale', 'email',
+        ];
         $parameters = [
             'access_token'    => $this->getStoredData('access_token'),
             'application_key' => $this->config->get('keys')['key'],
             'method'          => 'users.getCurrentUser',
             'fields'          => implode(',', $fields),
-            'sig'             => $sig,
         ];
+        $parameters['sig'] = $this->calculateSignature($parameters);
 
         $response = $this->apiRequest('fb.do', 'GET', $parameters);
 
@@ -86,15 +105,25 @@ class Odnoklassniki extends OAuth2
         }
 
         $userProfile = new User\Profile();
-
-
         $userProfile->identifier  = $data->get('uid');
-        $userProfile->email       = $data->get('email');
+        $userProfile->profileURL  = $data->get('url_profile');
+        $userProfile->photoURL    = $data->get('pic1024x768');
+        $userProfile->displayName = $data->get('name');
         $userProfile->firstName   = $data->get('first_name');
         $userProfile->lastName    = $data->get('last_name');
-        $userProfile->displayName = $data->get('name');
-        $userProfile->photoURL    = $data->get('pic1024x768');
-        $userProfile->profileURL  = 'http://ok.ru/profile/' . $data->get('uid');
+        $userProfile->gender      = $data->get('gender');
+        $userProfile->language    = $data->get('locale');
+        $userProfile->age         = $data->get('age');
+        $userProfile->email       = $data->get('email');
+        if ($data->get('birthday')) {
+            list($userProfile->birthYear, $userProfile->birthMonth, $profile->birthDay) = explode('-', $data->get('birthday'));
+        }
+        if ($address = $data->get('location')) {
+            $userProfile->address = implode(', ', $address);
+            $userProfile->country = isset($address['country']) ? $address['country'] : null;
+            $userProfile->region  = isset($address['region']) ? $address['region'] : null;
+            $userProfile->city    = isset($address['city']) ? $address['city'] : null;
+        }
 
         return $userProfile;
     }
