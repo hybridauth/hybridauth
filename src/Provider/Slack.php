b5eee7c17c76cf05ecba74d3f627643e0ca0b376
Christoph Böhme <christoph@b3e.net>
2019-12-16 19:22:56 +0100

    Fix invalid regex
    The regex for extracting the link to the largest image was not correct.

--- src/Provider/Slack.php
+++ src/Provider/Slack.php
@@ -69,7 +69,7 @@ class Slack extends OAuth2
     {
         $maxSize = 0;
         foreach ($data->filter('user')->properties() as $property) {
-            if (preg_match('^image_(\\d+)$', $property, $matches) === 1) {
+            if (preg_match('/^image_(\d+)$/', $property, $matches) === 1) {
                 $availableSize = (int) $matches[1];
                 if ($maxSize < $availableSize) {
                     $maxSize = $availableSize;
