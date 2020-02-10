09bf22c47b93cb6408baf7e3257217b946eb1027
Oleg Kuzava <olegkuzava@gmail.com>
2020-02-10 14:24:13 +0200

    Code style fixes

--- src/Hybridauth.php
+++ src/Hybridauth.php
@@ -166,12 +166,11 @@ class Hybridauth
         }
 
         $config = $providersConfig[$name];
-        
-        $config = $config + array(
-            'debug_mode'=> $this->config['debug_mode'],
-            'debug_file'=> $this->config['debug_file']
-        );
-        
+        $config += [
+            'debug_mode' => $this->config['debug_mode'],
+            'debug_file' => $this->config['debug_file'],
+        ];
+
         if (! isset($config['callback']) && isset($this->config['callback'])) {
             $config['callback'] = $this->config['callback'];
         }
