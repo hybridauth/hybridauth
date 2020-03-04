3b6bb274968de0398a798850204445152fcf1909
Oleg Kuzava <olegkuzava@gmail.com>
2020-03-04 15:56:14 +0200

    Make phpcs happy

--- src/Provider/Slack.php
+++ src/Provider/Slack.php
@@ -17,71 +17,77 @@ use Hybridauth\User;
  */
 class Slack extends OAuth2
 {
+
     /**
-    * {@inheritdoc}
-    */
+     * {@inheritdoc}
+     */
     public $scope = 'identity.basic identity.email identity.avatar';
 
     /**
-    * {@inheritdoc}
-    */
+     * {@inheritdoc}
+     */
     protected $apiBaseUrl = 'https://slack.com/';
 
     /**
-    * {@inheritdoc}
-    */
+     * {@inheritdoc}
+     */
     protected $authorizeUrl = 'https://slack.com/oauth/authorize';
 
     /**
-    * {@inheritdoc}
-    */
+     * {@inheritdoc}
+     */
     protected $accessTokenUrl = 'https://slack.com/api/oauth.access';
 
     /**
-    * {@inheritdoc}
-    */
+     * {@inheritdoc}
+     */
     protected $apiDocumentation = 'https://api.slack.com/docs/sign-in-with-slack';
 
     /**
-    * {@inheritdoc}
-    */
+     * {@inheritdoc}
+     */
     public function getUserProfile()
     {
         $response = $this->apiRequest('api/users.identity');
 
         $data = new Data\Collection($response);
 
-        if (! $data->exists('ok') || ! $data->get('ok')) {
+        if (!$data->exists('ok') || !$data->get('ok')) {
             throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
         }
 
         $userProfile = new User\Profile();
 
-        $userProfile->identifier  = $data->filter('user')->get('id');
+        $userProfile->identifier = $data->filter('user')->get('id');
         $userProfile->displayName = $data->filter('user')->get('name');
-        $userProfile->email       = $data->filter('user')->get('email');
-        $userProfile->photoURL    = $this->findLargestImage($data);
+        $userProfile->email = $data->filter('user')->get('email');
+        $userProfile->photoURL = $this->findLargestImage($data);
 
         return $userProfile;
     }
 
     /**
-    * Returns the url of the image with the highest resolution in the user object.
-    *
-    * Slack sends multiple image urls with different resolutions. As they make no guarantees
-    * which resolutions will be included we have to search all <code>image_*</code> properties
-    * for the one with the highest resolution. The resolution is attached to the property
-     * name such as <code>image_32</code> or <code>image_192</code>.
-    *
-    * @param Data\Collection $data response object as returned by <code>api/users.identity</code>
-    * @return string|null the value of the <code>image_*</code> property with the highest resolution.
-    */
+     * Returns the url of the image with the highest resolution in the user
+     * object.
+     *
+     * Slack sends multiple image urls with different resolutions. As they make
+     * no guarantees which resolutions will be included we have to search all
+     * <code>image_*</code> properties for the one with the highest resolution.
+     * The resolution is attached to the property name such as
+     * <code>image_32</code> or <code>image_192</code>.
+     *
+     * @param Data\Collection $data response object as returned by
+     *     <code>api/users.identity</code>
+     *
+     * @return string|null the value of the <code>image_*</code> property with
+     *     the highest resolution.
+     */
     private function findLargestImage(Data\Collection $data)
     {
         $maxSize = 0;
         foreach ($data->filter('user')->properties() as $property) {
             if (preg_match('/^image_(\d+)$/', $property, $matches) === 1) {
-                $availableSize = (int) $matches[1];
+                $availableSize = (int)$matches[1];
                 if ($maxSize < $availableSize) {
                     $maxSize = $availableSize;
                 }
@@ -92,5 +98,4 @@ class Slack extends OAuth2
         }
         return null;
     }
-
 }
