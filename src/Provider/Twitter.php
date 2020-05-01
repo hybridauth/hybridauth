4aaf0c19ec42da46cd6a04f4d5ee76d8b5f22525
Martin Labelle <martin.labelle@sppml.com>
2020-05-01 11:59:23 -0400

    Fix error : code":32,"message":"Could not authenticate you."

--- src/Provider/Twitter.php
+++ src/Provider/Twitter.php
@@ -81,7 +81,7 @@ class Twitter extends OAuth1
      */
     public function getUserProfile()
     {
-        $response = $this->apiRequest('account/verify_credentials.json?include_email=true');
+        $response = $this->apiRequest('account/verify_credentials.json', 'GET', ['include_email' => $this->config->get('include_email') === false ? 'false' : 'true',]);
 
         $data = new Data\Collection($response);
 
