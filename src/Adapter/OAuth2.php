e61936fc2d673ddbc0736c40678d97848389e096
Oliver Jusinger <oliver.jusinger@bergfex.at>
2020-05-21 14:35:35 +0200

    phpdoc for AuthorizeUrlParameters fixed

--- src/Adapter/OAuth2.php
+++ src/Adapter/OAuth2.php
@@ -126,7 +126,7 @@ abstract class OAuth2 extends AbstractAdapter implements AdapterInterface
     /**
     * Authorization Url Parameters
     *
-    * @var string
+    * @var array
     */
     protected $AuthorizeUrlParameters = [];
 
