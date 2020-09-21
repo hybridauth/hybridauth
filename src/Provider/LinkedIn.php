59e842d04e68c4000c73fda38d44c5909aff34e5
Oleg Kuzava <olegkuzava@gmail.com>
2020-09-21 21:14:15 +0300

    fix ci

--- src/Provider/LinkedIn.php
+++ src/Provider/LinkedIn.php
@@ -123,8 +123,11 @@ class LinkedIn extends OAuth2
      */
     public function getUserEmail()
     {
-        $response = $this->apiRequest('emailAddress', 'GET', [ 'q' => 'members', 'projection' => '(elements*(handle~))' ]);
-        $data     = new Data\Collection($response);
+        $response = $this->apiRequest('emailAddress', 'GET', [
+            'q' => 'members',
+            'projection' => '(elements*(handle~))',
+        ]);
+        $data = new Data\Collection($response);
 
         foreach ($data->filter('elements')->toArray() as $element) {
             $item = new Data\Collection($element);
