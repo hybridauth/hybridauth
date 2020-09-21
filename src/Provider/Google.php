b258815388d6038878682500c318c435b8b9ecc3
Oleg Kuzava <olegkuzava@gmail.com>
2020-09-21 16:36:38 +0300

    fix ci

--- src/Provider/Google.php
+++ src/Provider/Google.php
@@ -50,6 +50,7 @@ class Google extends OAuth2
     /**
     * {@inheritdoc}
     */
+    // phpcs:ignore
     protected $scope = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
 
     /**
