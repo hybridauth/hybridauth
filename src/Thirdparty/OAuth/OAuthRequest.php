731235163453610d15d6dc73f54012e9b6d6776a
Oleg Kuzava <olegkuzava@gmail.com>
2021-01-19 23:24:31 +0200

    Update OAuthRequest.php

--- src/Thirdparty/OAuth/OAuthRequest.php
+++ src/Thirdparty/OAuth/OAuthRequest.php
@@ -168,21 +168,19 @@ class OAuthRequest
         $params = [];
 
         // Grab all parameters.
-        foreach( $this->parameters as $key_param => $value_param ) {
-            
-            // Avoid arrays because it causes duplicate keys
-            if( is_array( $value_param ) )
-                continue;
-
-            $params[$key_param] = $value_param;
+        foreach ($this->parameters as $key_param => $value_param) {
+            // Process only scalar values.
+            if (is_scalar($value_param)) {
+                $params[$key_param] = $value_param;
+            }
         }
-        
+
         // Remove oauth_signature if present
         // Ref: Spec: 9.1.1 ("The oauth_signature parameter MUST be excluded.")
         if (isset($params['oauth_signature'])) {
             unset($params['oauth_signature']);
         }
-        
+
         return OAuthUtil::build_http_query($params);
     }
     
