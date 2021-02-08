5f753697fd9e4bfa120321334cf628f9472a2143
Patrick Groot <info@patrickgroot.com>
2021-02-08 14:11:16 +0100

    Update Yandex.php

--- src/Provider/Yandex.php
+++ src/Provider/Yandex.php
@@ -10,8 +10,8 @@ namespace Hybridauth\Provider;
 use Hybridauth\Adapter\OAuth2;
 use Hybridauth\Exception\Exception;
 use Hybridauth\Exception\UnexpectedApiResponseException;
-use Hybridauth\Data\Collection;
-use Hybridauth\User\Profile;
+use Hybridauth\Data;
+use Hybridauth\User;
 
 /**
  * Yandex OAuth2 provider adapter.
@@ -48,25 +48,24 @@ class Yandex extends OAuth2
         $this->scope = implode(',', []);
 
         $response = $this->apiRequest($this->apiBaseUrl, 'GET', ['format' => 'json']);
-
+		
         if (!isset($response->id)) {
             throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
         }
 
-        $data = new Collection($response);
-
+        $data = new Data\Collection($response);
+		
         if (!$data->exists('id')) {
             throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
         }
-
-        $userProfile = new Profile();
-
+		
+        $userProfile = new User\Profile();
+		
         $userProfile->identifier = $data->get('id');
-        $userProfile->firstName = $data->get('real_name');
-        $userProfile->lastName = $data->get('family_name');
+		$userProfile->firstName = $data->get('first_name');
+        $userProfile->lastName = $data->get('last_name');
         $userProfile->displayName = $data->get('display_name');
-        $userProfile->photoURL = 'http://upics.yandex.net/' . $userProfile->identifier . '/normal';
-        $userProfile->profileURL = "";
+        $userProfile->photoURL = 'https://avatars.yandex.net/get-yapic/' . $data->get('default_avatar_id') . '/islands-200';
         $userProfile->gender = $data->get('sex');
         $userProfile->email = $data->get('default_email');
         $userProfile->emailVerified = $data->get('default_email');
