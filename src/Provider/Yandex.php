971c3b30f1f6ed27f359944e232db80e58bf68c3
Patrick Groot <info@patrickgroot.com>
2021-02-08 14:09:00 +0100

    Pushing fixes yandex
    Fixes #1279 and first and last name.

--- src/Provider/Yandex.php
+++ src/Provider/Yandex.php
@@ -10,31 +10,11 @@ namespace Hybridauth\Provider;
 use Hybridauth\Adapter\OAuth2;
 use Hybridauth\Exception\Exception;
 use Hybridauth\Exception\UnexpectedApiResponseException;
-use Hybridauth\Data\Collection;
-use Hybridauth\User\Profile;
+use Hybridauth\Data;
+use Hybridauth\User;
 
 /**
- * Yandex provider adapter.
- *
- * Example:
- *
- *   $config = [
- *       'callback'  => Hybridauth\HttpClient\Util::getCurrentUrl(),
- *       'keys'      => ['id' => '', 'secret' => ''],
- *   ];
- *
- *   $adapter = new Hybridauth\Provider\Yandex($config);
- *
- *   try {
- *       if (!$adapter->isConnected()) {
- *           $adapter->authenticate();
- *       }
- *
- *       $userProfile = $adapter->getUserProfile();
- *   }
- *   catch(\Exception $e) {
- *       print $e->getMessage() ;
- *   }
+ * Yandex OAuth2 provider adapter.
  */
 class Yandex extends OAuth2
 {
@@ -54,34 +34,38 @@ class Yandex extends OAuth2
     protected $accessTokenUrl = 'https://oauth.yandex.ru/token';
 
     /**
+     * {@inheritdoc}
+     */
+    protected $apiDocumentation = 'https://yandex.com/dev/oauth/doc/dg/concepts/about-docpage/';
+
+    /**
      * load the user profile from the IDp api client
      *
      * @throws Exception
      */
     public function getUserProfile()
     {
-
         $this->scope = implode(',', []);
 
-        $response = $this->apiRequest($this->apiBaseUrl . "?format=json");
-
+        $response = $this->apiRequest($this->apiBaseUrl, 'GET', ['format' => 'json']);
+		
         if (!isset($response->id)) {
             throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
         }
 
-        $data = new Collection($response);
-
+        $data = new Data\Collection($response);
+		
         if (!$data->exists('id')) {
             throw new UnexpectedApiResponseException('Provider API returned an unexpected response.');
         }
-
-        $userProfile = new Profile();
-
+		
+        $userProfile = new User\Profile();
+		
         $userProfile->identifier = $data->get('id');
-        $userProfile->firstName = $data->get('real_name');
-        $userProfile->lastName = $data->get('family_name');
+		$userProfile->firstName = $data->get('first_name');
+        $userProfile->lastName = $data->get('last_name');
         $userProfile->displayName = $data->get('display_name');
-        $userProfile->photoURL = 'http://upics.yandex.net/' . $userProfile->identifier . '/normal';
+        $userProfile->photoURL = 'https://avatars.yandex.net/get-yapic/' . $data->get('default_avatar_id') . '/islands-200';
         $userProfile->profileURL = "";
         $userProfile->gender = $data->get('sex');
         $userProfile->email = $data->get('default_email');
