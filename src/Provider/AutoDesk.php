cec338e17df09d09b64850b0faa025a89a533ad7
Patrick Groot <info@patrickgroot.com>
2021-03-13 16:43:11 +0100

    Update AutoDesk.php

--- src/Provider/AutoDesk.php
+++ src/Provider/AutoDesk.php
@@ -1,5 +1,5 @@
 <?php
-/*!
+/**
  * Hybridauth
  * https://hybridauth.github.io | https://github.com/hybridauth/hybridauth
  *  (c) 2020 Hybridauth authors | https://hybridauth.github.io/license.html
