92b5e1c0059c72017b5f1a8c86e2a8bde4ff8fa1
Oliver Jusinger <oliver.jusinger@bergfex.at>
2022-06-14 17:31:01 +0200

    Provide support for Firebase/JWT version 6.2

--- src/Provider/Apple.php
+++ src/Provider/Apple.php
@@ -7,6 +7,7 @@
 
 namespace Hybridauth\Provider;
 
+use Composer\InstalledVersions;
 use Exception;
 use Firebase\JWT\ExpiredException;
 use Hybridauth\Exception\InvalidApplicationCredentialsException;
@@ -20,6 +21,7 @@ use phpseclib\Crypt\RSA;
 use phpseclib\Math\BigInteger;
 
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 /**
  * Apple OAuth2 provider adapter.
@@ -187,7 +189,9 @@ class Apple extends OAuth2
                     );
                     $pem = $rsa->getPublicKey();
 
-                    $payload = JWT::decode($id_token, $pem, ['RS256']);
+                    $payload = ($this->getJwtVersion() < '6.2') ?
+                        JWT::decode($id_token, $pem, ['RS256']) :
+                        JWT::decode($id_token, new Key($pem, 'RS256'));
                     break;
                 } catch (Exception $e) {
                     $error = $e->getMessage();
@@ -283,5 +287,10 @@ class Apple extends OAuth2
         $secret = JWT::encode($data, $key_content, 'ES256', $key_id);
 
         return $secret;
+    }
+
+    private function getJwtVersion()
+    {
+        return InstalledVersions::getVersion('firebase/php-jwt');
     }
 }
